b13ea7e6528555d68ca8a64cb14aab9ec28dbf46
Thomas D'Astolto <thom@macbookair.speedport.ip>
2024-11-29 10:36:02 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,42 @@
+console.log("starting javascript");
+
+// Exercise 1 //
+
+const myName = "Thomas"
+console.log(myName);
+
+// Exercise 2 //
+
+const myAge = 31
+console.log(myAge);
+
+// Exercise 3 //
+
+const juliaAge = 32
+const ageDiff = juliaAge - myAge
+console.log(ageDiff);
+
+// Exercise 4 //
+
+if (myAge > 21) {
+    console.log("You are older than 21")
+} else if (myAge < 21) {
+    console.log ("You are not older than 21")
+}
+
+// Exercise 5 //
+
+if (myAge > juliaAge) {
+    console.log("Julia is younger than you")
+} else if (myAge < juliaAge) {
+    console.log ("Julia is older than you")
+} else if (myAge === juliaAge) {
+    console.log ("You have the same age as Julia")
+}
+
+// Exercise 6 //
+
+// const classNames = ["Thomas", "Dima", "Jonas", "Maria", "Diogo", "Raul", "Hassan"];
+// console.log(classNames[0]);
+// console.log(classNames[6]);
+// for ()
