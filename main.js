2e1480ff2d23c733fbda0c74c97c90e1f0d36685
TheCodingTom <dastolto.t@gmail.com>
2024-11-29 14:11:44 +0100

    2 more JS exercise 2 more JS exercise

--- main.js
+++ main.js
@@ -42,3 +42,21 @@ console.log(classNames[6]);
 for (j = 0; j < classNames.length; j++) {
     console.log(classNames[j])
 }
+
+// Exercise 7 // Need to add conditional to print only even number
+
+// const studentsAge = [31, 33, 18, 28, 30, 40, 29"];
+// while (j < studentsAge.length) {
+//     console.log(studentsAge[j]);
+//     j++
+// }
+
+// Exercise 8 // 
+// The sum of the array of numbers is calculated by looping through the array and adding the value of each array element to a variable called sum.//
+
+const studentsAge = [31, 33, 18, 28, 30, 40, 29];
+let sum = 0;
+for (j = 0; j < studentsAge.length; j++) { 
+    sum = sum += studentsAge[j];
+}
+console.log(sum)
